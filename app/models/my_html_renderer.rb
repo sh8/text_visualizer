c386ca6f7afa2688c608b6ea01ce891843dcca09
Shune <shun3475.innobeta@gmail.com>
2014-05-04 23:43:33 +0900

    modify my_html_renderer

--- app/models/my_html_renderer.rb
+++ app/models/my_html_renderer.rb
@@ -4,35 +4,42 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
     	if header_level == 1 
         if @count == nil
           @count = true
-          s = "<div><h#{header_level}>#{text}</h#{header_level}>"
+          s = "<div class='container'><div class='box column2'><h#{header_level}>#{text}</h#{header_level}></div>"
           return s
         end
         
-    		s = "</div>\n<div><h#{header_level}>#{text}</h#{header_level}>"
+    		s = "</div>\n<div class='container'><div class='box column2'><h#{header_level}>#{text}</h#{header_level}></div>"
           
         return s
     	end
 
     	if header_level == 2
-    		s = "<h#{header_level}>#{text}</h#{header_level}>"
+    		s = "<div class='box column2'><h#{header_level}>#{text}</h#{header_level}></div>"
     		return s
     	end
 
     	if header_level == 3
-    		s = "<h#{header_level}>#{text}</h#{header_level}>"
+    		s = "<div class='box column2'><h#{header_level}>#{text}</h#{header_level}></div>"
     		return s
     	end
 
     	if header_level == 4
-    		s = "<h#{header_level}>#{text}</h#{header_level}>"
+    		s = "<div class='box column2'><h#{header_level}>#{text}</h#{header_level}></div>"
     		return s
     	end
 
   end
 
-
-
   def new_div(text)
-
+    
+  end
+  
+  def list(contents, list_type)
+    case list_type
+       when :ordered
+         "<div class='box column2'>\n\n.nr step 0 1\n#{contents}\n</div>"
+       when :unordered
+         "\n<div class='box column2'>\n#{contents}\n</div>"
+       end
   end
 end
