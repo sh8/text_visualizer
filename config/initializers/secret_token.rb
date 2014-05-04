8f43316c42aa68daed3530317c7fa78443d8169f
岩瀬　駿 <shun@iwaseshun-no-MacBook-Pro.local>
2014-05-04 10:16:57 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TextVisualizer::Application.config.secret_key_base = '5a2f801392a7014fd382f85d4b3f5670ccc1bb8e63cdc94742eabc6f504c4cd5f649dfdf0a76992d1a402d73982231b4479626b5a7be1d5869aff3266511ca0f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TextVisualizer::Application.config.session_store :cookie_store, key: '_text_visualizer_session'
