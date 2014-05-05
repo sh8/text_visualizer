078b51dc09305648ab6b3defa712c26fff1ad91a
Shune <shun3475.innobeta@gmail.com>
2014-05-05 13:43:14 +0900

    before merge

--- app/models/my_html_renderer.rb
+++ app/models/my_html_renderer.rb
@@ -1,6 +1,7 @@
 class MyHTMLRenderer < Redcarpet::Render::HTML
   def header(text, header_level, options)
     
+
       if header_level == 1 
         if @h1_is_open == nil
           @h1_is_open = true
@@ -10,11 +11,12 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
 
         s = "</div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}>"
         return s
+
       end
 
       if header_level == 2
         if @h2_is_open == nil
-          @h2_is_open = true
+          @h2_is_open = true  
           s = "<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
           return s
         end
@@ -22,11 +24,11 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
           s = "</div>\n<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
           return s
       end
-
       if header_level == 3
         s = "<div class='h3bracket'><div class='h3item h3box'><h#{header_level}>#{text}</h#{header_level}></div></div>"
         return s
       end
+
 
       if header_level == 4
         s = "<div class='box column2'><h#{header_level}>#{text}</h#{header_level}></div>"
@@ -44,7 +46,7 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
        when :ordered
          "<div class='h2item  h3bracket'><p>\n\n.nr step 0 1\n#{contents}\n</p></div>\n"
        when :unordered
-         "<div class='h2item h3bracket'><p>\n.\n#{contents}</p></div>\n"
+         "<div class='h2item h3bracket'><p>\n\n#{contents}</p></div>\n"
        end
   end
 end
