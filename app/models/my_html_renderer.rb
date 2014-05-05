fbb6574c82a8ae5d481d82e2b411cfa1f9b00d72
Yu Asabe <lajolla2005@gmail.com>
2014-05-05 09:06:05 +0900

    Updated myhtmlrenderer and coresponding designs.

--- app/models/my_html_renderer.rb
+++ app/models/my_html_renderer.rb
@@ -3,33 +3,64 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
   def header(text, header_level, options)
     
     	if header_level == 1 
+        if @h3_opened == true
+          a = "</div>"
+          @h3_opened = nil
+        end
+
         if @count == nil
           @count = true
           s = "<div class='item'><h#{header_level}>#{text}</h#{header_level}>"
-          return s
+          return a.to_s() + s
         end
         
-    		s = "</div>\n<div class='item'><h#{header_level}>#{text}</h#{header_level}>"
+    		s = "</div></div>\n<div class='item'><h#{header_level}>#{text}</h#{header_level}>"
           
         return s
     	end
 
     	if header_level == 2
+        if @h3_opened == true
+          a = "</div>"
+          @h3_opened = nil
+        end
         
   		  s = "<div class='item2'><h#{header_level}>#{text}</h#{header_level}></div>"
-  		  return s
+  		  return a.to_s() + s
     	end
 
     	if header_level == 3
-    		s = "<div class='item3'><h#{header_level}>#{text}</h#{header_level}></div>"
-    		return s
+        if @h3_opened == true
+          a = "</div><div class='item3'><h#{header_level}>#{text}</h#{header_level}>"
+          #@h3_opened = nil
+          return a
+        else #h3_opened = nil
+          s = "<div class='item3'><h#{header_level}>#{text}</h#{header_level}>"
+          @h3_opened = true
+          return s
+        end
+        
+        
+
+        # if @h3_opened == nil 
+        #   s = "<div class='item3'><h#{header_level}>#{text}</h#{header_level}>"
+        #   @h3_opened = true
+        #   return a.to_s() + s
+        # end
+    		
     	end
 
     	if header_level == 4
+        if @h3_opened == true
+          a = "</div>"
+          @h3_opened = nil
+        end
+
     		s = "<h#{header_level}>#{text}</h#{header_level}>"
-    		return s
+    		return a.to_s() + s
     	end
 
+      
   end
 
   def list(contents, list_type)
