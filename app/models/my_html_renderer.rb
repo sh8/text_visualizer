828f6afcefacd37efdff6873fadb204c0381e0f4
Moe Sunami <17sunamim@gmail.com>
2014-05-05 11:06:39 +0900

    Added fonts and weird formatting and css and lots of divs

--- app/models/my_html_renderer.rb
+++ app/models/my_html_renderer.rb
@@ -1,38 +1,55 @@
 class MyHTMLRenderer < Redcarpet::Render::HTML
   def header(text, header_level, options)
     
-    	if header_level == 1 
-        if @count == nil
-          @count = true
-          s = "<div><h#{header_level}>#{text}</h#{header_level}>"
+      if header_level == 1 
+        if @h1_is_open == nil
+          @h1_is_open = true
+          s = "<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
           return s
         end
-        
-    		s = "</div>\n<div><h#{header_level}>#{text}</h#{header_level}>"
-          
+
+        s = "</div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}>"
         return s
-    	end
 
-    	if header_level == 2
-    		s = "<h#{header_level}>#{text}</h#{header_level}>"
-    		return s
-    	end
+      end
 
-    	if header_level == 3
-    		s = "<h#{header_level}>#{text}</h#{header_level}>"
-    		return s
-    	end
+      if header_level == 2
+        if @h2_is_open == nil
+          @h2_is_open = true
+          s = "<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
+          return s
+        end
 
-    	if header_level == 4
-    		s = "<h#{header_level}>#{text}</h#{header_level}>"
-    		return s
-    	end
+          s = "</div>\n<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
+          return s
+      end
+
+      if header_level == 3
+        s = "<div class='h3bracket'><div class='h3item h3box'><h#{header_level}>#{text}</h#{header_level}></div></div>"
+        return s
+      end
+
+      if header_level == 4
+        s = "<div class='box column2'><h#{header_level}>#{text}</h#{header_level}></div>"
+        return s
+      end
 
   end
-
-
 
   def new_div(text)
-
+    
   end
+  
+  def list(contents, list_type)
+    case list_type
+       when :ordered
+         "<div class='h2item  h3bracket'><p>\n\n.nr step 0 1\n#{contents}\n</p></div>\n"
+       when :unordered
+         "<div class='h2item h3bracket'><p>\n.\n#{contents}</p></div>\n"
+       end
+  end
+end
+
+def doc_footer()
+
 end
