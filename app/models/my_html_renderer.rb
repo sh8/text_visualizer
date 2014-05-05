81fea2882064edbb553cdb96d1068b58d71abf0a
Moe Sunami <17sunamim@gmail.com>
2014-05-05 14:56:12 +0900

    heyhey what is up yo i changed the margins yo

--- app/models/my_html_renderer.rb
+++ app/models/my_html_renderer.rb
@@ -1,6 +1,8 @@
 class MyHTMLRenderer < Redcarpet::Render::HTML
   def header(text, header_level, options)
+
     
+
       if header_level == 1 
         if @h1_is_open == nil
           @h1_is_open = true
@@ -8,7 +10,20 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
           return s
         end
 
-        s = "</div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}>"
+        if @h3_is_open == true
+          s = "<!--if h3 was open before --></div></div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
+          @h2_is_open = false
+          @h3_is_open = false
+          return s
+        end
+
+        if @h2_is_open == true
+          s = "<!--if h2 was open before --></div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
+          @h2_is_open = false
+          return s
+        end
+
+        s = "<!-- closes h1 --></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
         return s
       end
 
@@ -19,8 +34,10 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
           return s
         end
 
-          s = "</div>\n<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
-          return s
+        s = "</div>\n<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
+        @h2_is_open = true
+
+        return s
       end
 
       if header_level == 3
@@ -33,6 +50,16 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
         return s
       end
 
+      def open_brackets
+        if nil
+          {1 => 0, 2 => 0, 3 => 0}
+        end
+      end
+
+      def previous_header_num
+
+      end
+
   end
 
   def new_div(text)
@@ -42,9 +69,9 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
   def list(contents, list_type)
     case list_type
        when :ordered
-         "<div class='h2item  h3bracket'><p>\n\n.nr step 0 1\n#{contents}\n</p></div>\n"
+         "<div class='h2item  h3bracket bullets'><p>\n\n.nr step 0 1\n#{contents}\n</p></div>\n"
        when :unordered
-         "<div class='h2item h3bracket'><p>\n.\n#{contents}</p></div>\n"
+         "<div class='h2item h3bracket bullets'><p>\n.\n#{contents}</p></div>\n"
        end
   end
 end
