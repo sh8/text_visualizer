f6d0b7d411588302eed83049d20b23d8d4abb8a1
Yu Asabe <lajolla2005@gmail.com>
2014-05-06 10:31:12 +0900

    Pretty much finished with the main features.

--- app/models/my_html_renderer.rb
+++ app/models/my_html_renderer.rb
@@ -2,27 +2,14 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
   def header(text, header_level, options)
 
 
-      if header_level == 1 
+    if header_level == 1 
         if @h1_is_open == nil
           @h1_is_open = true
           s = "<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
           return s
         end
 
-        if @h3_is_open == true
-          s = "<!--if h3 was open before --></div></div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
-          @h2_is_open = false
-          @h3_is_open = false
-          return s
-        end
-
-        if @h2_is_open == true
-          s = "<!--if h2 was open before --></div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
-          @h2_is_open = false
-          return s
-        end
-
-        s = "<!-- closes h1 --></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}></div>"
+        s = "</div></div>\n<div class='h1bracket'><div class='h1item h1box'><h#{header_level}>#{text}</h#{header_level}>"
         return s
 
       end
@@ -30,17 +17,15 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
       if header_level == 2
         if @h2_is_open == nil
           @h2_is_open = true  
-          s = "<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div><br><br>"
+          s = "<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
           return s
         end
 
-        s = "</div>\n<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div><br><br>"
-        @h2_is_open = true
-
-        return s
+          s = "</div>\n<div class='h2bracket'><div class='h2item h2box'><h#{header_level}>#{text}</h#{header_level}></div>"
+          return s
       end
       if header_level == 3
-        s = "<br><br><div class='h3bracket'><div class='h3item h3box'><br><br><h#{header_level}>#{text}</h#{header_level}></div></div>"
+        s = "<div class='h3bracket'><div class='h3item h3box'><h#{header_level}>#{text}</h#{header_level}></div></div>"
         return s
       end
 
@@ -50,15 +35,15 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
         return s
       end
 
-      def open_brackets
-        if nil
-          {1 => 0, 2 => 0, 3 => 0}
-        end
-      end
+      # def open_brackets
+      #   if nil
+      #     {1 => 0, 2 => 0, 3 => 0}
+      #   end
+      # end
 
-      def previous_header_num
+      # def previous_header_num
 
-      end
+      # end
 
   end
 
@@ -74,9 +59,9 @@ class MyHTMLRenderer < Redcarpet::Render::HTML
   def list_item(text, list_type)
     case list_type
        when :ordered
-         "<div class='h2item  h3bracket bullets'><br><br><p>\n\n.nr step 0 1\n#{contents}\n</p></div>\n<br><br>"
+         "<div class='h2item  h3bracket bullets'><p>\n\n.nr step 0 1\n#{contents}\n</p></div>\n"
        when :unordered
-         "<div class='h2item h3bracket'><br><br><p>\n\n#{text}</p></div>\n<br><br>"
+         "<div class='h2item h3bracket'><p>\n\n#{text}</p></div>\n"
        end
   end
 
